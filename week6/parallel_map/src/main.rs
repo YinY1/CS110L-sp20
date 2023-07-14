3bc7ffc4ad2b474d39e6e38c3ea15c2461b626f3
Kal'tsit <36351545+YinY1@users.noreply.github.com>
2023-07-14 19:02:38 +0800

    week6

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -1,4 +1,3 @@
-use crossbeam_channel;
 use std::{thread, time};
 
 fn parallel_map<T, U, F>(mut input_vec: Vec<T>, num_threads: usize, f: F) -> Vec<U>
@@ -8,7 +7,44 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+
+    for _ in 0..input_vec.len() {
+        output_vec.push(Default::default());
+    }
+    let (sender_raw, receiver_raw) = crossbeam_channel::unbounded();
+    let (sender_result, receiver_result) = crossbeam_channel::unbounded();
+    let mut threads = vec![];
+    for _ in 0..num_threads {
+        let recv_raw = receiver_raw.clone();
+        let send_res = sender_result.clone();
+        threads.push(thread::spawn(move || {
+            while let Ok((num, idx)) = recv_raw.recv() {
+                send_res
+                    .send((f(num), idx))
+                    .expect("Tried writing to channel, but there are no receivers!");
+            }
+        }));
+    }
+
+    drop(sender_result);
+
+    let mut i = input_vec.len();
+    while let Some(num) = input_vec.pop() {
+        i -= 1;
+        sender_raw
+            .send((num, i))
+            .expect("Tried writing to channel, but there are no receivers!");
+    }
+
+    drop(sender_raw);
+
+    while let Ok((res, idx)) = receiver_result.recv() {
+        output_vec[idx] = res;
+    }
+
+    for thread in threads {
+        thread.join().expect("Panic occurred in thread");
+    }
     output_vec
 }
 
